b974c73d5719ebbd7bffb2fda3ddc0c1687756f9
Felipe Campos <felipecamposmacedo@gmail.com>
2024-09-12 02:35:15 -0300

    CalcDefaultFinish

--- src/app/CalcDefault/CalcDefaultFunc.ts
+++ src/app/CalcDefault/CalcDefaultFunc.ts
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function useCalc() {
-  const [inputValue, setInputValue] = useState('');
+  const [value, setInputValue] = useState<string>("0");
+  const [previousNumber, setPreviousNumber] = useState<number | null>(null);
+  const [operator, setOperator] = useState<string | null>(null);
+  const displayRef = useRef<HTMLDivElement>(null);
 
   function handleButtonClick(value: string) {
     switch (value) {
@@ -18,7 +21,28 @@ export default function useCalc() {
       case "8":
       case "9":
       case "0":
+      case ".":
         AddDisplay(value);
+        break;
+      case "CE":
+        clearDisplayAndMemory();
+        break;
+      case "BACK":
+        backspaceNumber();
+        break;
+      case "-":
+      case "+":
+      case "x":
+      case "/":
+        setOperatorAndStorePreviousNumber(value);
+        break;
+      case "sqrt":
+      case "X^2":
+      case "1/X":
+        performSingleOperandOperation(value);
+        break;
+      case "=":
+        calculate();
         break;
       default:
         console.log("Operação não reconhecida");
@@ -29,14 +53,108 @@ export default function useCalc() {
   }
 
   function AddDisplay(value: string) {
-    let display = document.getElementById("display");
-    display && (display.innerHTML += value);
+    if (displayRef.current) {
+      let valueDisplay = displayRef.current.innerHTML;
+      if (valueDisplay === "0") {
+        if (value === ".") {
+          value = "0.";
+        }
+        displayRef.current.innerHTML = value;
+      } else {
+        displayRef.current.innerHTML += value;
+      }
+    }
   }
 
   function clearDisplay() {
-    let display = document.getElementById("display");
-    display && (display.innerHTML = "0");  
+    if (displayRef.current) {
+      displayRef.current.innerHTML = "0";
+    }
+    setInputValue("0");
   }
 
-  return { handleButtonClick };
+  function clearDisplayAndMemory() {
+    if (displayRef.current) {
+      displayRef.current.innerHTML = "0";
+    }
+    setInputValue("0");
+    setPreviousNumber(null);
+    setOperator(null);
+  }
+
+  function setOperatorAndStorePreviousNumber(op: string) {
+    if (displayRef.current) {
+      setPreviousNumber(Number(displayRef.current.innerHTML));
+      setOperator(op);
+      clearDisplay();
+    }
+  }
+
+  function performSingleOperandOperation(operation: string) {
+    if (displayRef.current) {
+      let currentNumber = Number(displayRef.current.innerHTML);
+      let result = 0;
+
+      switch (operation) {
+        case "sqrt":
+          result = Math.sqrt(currentNumber);
+          break;
+        case "X^2":
+          result = currentNumber * currentNumber;
+          break;
+        case "1/X":
+          result = 1 / currentNumber;
+          break;
+        default:
+          console.log("Operação não reconhecida");
+          return;
+      }
+
+      displayRef.current.innerHTML = result.toString();
+      setOperator(null); // Reseta o operador após a operação
+    }
+  }
+
+  function calculate() {
+    if (displayRef.current && previousNumber !== null && operator !== null) {
+      let currentNumber = Number(displayRef.current.innerHTML);
+      let result = 0;
+
+      switch (operator) {
+        case "+":
+          result = previousNumber + currentNumber;
+          break;
+        case "-":
+          result = previousNumber - currentNumber;
+          break;
+        case "x":
+          result = previousNumber * currentNumber;
+          break;
+        case "/":
+          result = previousNumber / currentNumber;
+          break;
+        default:
+          console.log("Operação não reconhecida");
+          return;
+      }
+
+      displayRef.current.innerHTML = result.toString();
+      setPreviousNumber(null);
+      setOperator(null);
+    }
+  }
+
+  function backspaceNumber() {
+    if (displayRef.current) {
+      let valueDisplay = displayRef.current.innerHTML;
+
+      if (valueDisplay.length > 1) {
+        displayRef.current.innerHTML = valueDisplay.slice(0, -1);
+      } else {
+        displayRef.current.innerHTML = "0";
+      }
+    }
+  }
+
+  return { handleButtonClick, displayRef };
 }
--- src/app/CalcDefault/page.tsx
+++ src/app/CalcDefault/page.tsx
@@ -2,18 +2,17 @@
 
 import { Button } from "@/components/ui/button";
 import { Card, CardDescription } from "@/components/ui/card";
-import Image from "next/image";
 import useCalc from "./CalcDefaultFunc";
-import { useState } from 'react';
+import { useRef } from 'react';
 
 export default function CalcDefault() {
-  const {handleButtonClick} = useCalc();
+  const {handleButtonClick, displayRef} = useCalc();
   return (
   <>
     <main className="flex flex-col items-center">
         <Card className="bg-gray-100">
           <CardDescription className="flex items-center justify-end h-16 m-4 rounded-lg text-3xl font-bold px-4 bg-white border-[1px]">
-            <span id="display">0</span>
+            <span ref={displayRef}>0</span>
           </CardDescription>
           <div className="grid grid-cols-4 items-center p-4 gap-1">
       {[
